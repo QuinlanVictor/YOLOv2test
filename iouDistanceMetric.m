6077dee483cd09b150dfa80e05a4285ebb5afa0d
QuinlanVictor <qianliang1994@126.com>
2019-10-25 14:26:18 +0800

    matlab自定义支持函数
    matlab自定义支持函数

---
+++ iouDistanceMetric.m
@@ -0,0 +1,14 @@
+function dist = iouDistanceMetric(boxWidthHeight,allBoxWidthHeight)
+% Return the IoU distance metric. The bboxOverlapRatio function
+% is used to produce the IoU scores. The output distance is equal
+% to 1 - IoU.
+
+% Add x and y coordinates to box widths and heights so that
+% bboxOverlapRatio can be used to compute IoU.
+boxWidthHeight = prefixXYCoordinates(boxWidthHeight);
+allBoxWidthHeight = prefixXYCoordinates(allBoxWidthHeight);
+
+% Compute IoU distance metric.
+dist = 1 - bboxOverlapRatio(allBoxWidthHeight, boxWidthHeight);
+end
+
