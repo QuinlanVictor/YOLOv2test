900cdfa8ea614999aa16d829339f984aac7141f5
QuinlanVictor <qianliang1994@126.com>
2019-11-07 16:26:20 +0800

    修正LIDC数据文件编号
    修正LIDC数据文件编号

--- xiuzhengID.m
+++ xiuzhengID.m
@@ -1,14 +1,34 @@
 clear;clc;
-cd
-folder_name_all= pwd
-%folder_name_all = uigetdir('');%选择文件夹
-img_path_list = dir(strcat(folder_name_all,'\','*.dcm'));% 获取该文件夹中所有格式的图像 
-for i=1:numel(img_path_list)
-    oldname = img_path_list(i).name;
-    dicomInformation = dicominfo(strcat(folder_name_all,'\',oldname));
-    imagenum = dicomInformation.InstanceNumber;
-    newname=num2str(imagenum,'%d.dcm');
-    %movefile(oldname,newname);
-    cmd=['rename ',oldname,' ', newname];
-    status = system(cmd);
+folder_name_all = uigetdir('');%选择文件夹
+filepathlist = dir(folder_name_all);
+diary 'worklog.txt'
+disp(' ');
+time=clock;
+t1=strcat(num2str(time(1)),'年',num2str(time(2)),'月',num2str(time(3)),'日',num2str(time(4)),'：',num2str(time(5)));
+disp(['北京时间:',num2str(t1),'开始重命名']);
+for j=24:length(filepathlist)
+%for j=3:4 %24是文件夹222
+    tic;
+    a=filepathlist(j).name;
+    subsublist=dir(strcat(folder_name_all,'\',a,'\','Dicom'));
+    b=subsublist(3).name;
+    dcm_name_all=num2str(strcat(folder_name_all,'\',a,'\','Dicom','\',b));
+    cd(dcm_name_all);
+    dcm_path_list = dir(strcat(folder_name_all,'\',a,'\','Dicom','\',b,'\','*.dcm'));
+    for i=1:numel(dcm_path_list)
+        oldname = dcm_path_list(i).name;
+        dicomInformation = dicominfo(strcat(dcm_name_all,'\',oldname));
+        imagenum = dicomInformation.InstanceNumber;
+        newname=num2str(imagenum,'%d.dcm');
+        cmd=['rename ',oldname,' ', newname];
+        status = system(cmd);
+        if status~=0
+        disp([num2str(a),'修改不成功!']);
+    end
+    end
+    disp([num2str(a),'重命名完毕，用时：',num2str(toc),'秒']);
 end
+time=clock;
+t2=strcat(num2str(time(1)),'年',num2str(time(2)),'月',num2str(time(3)),'日',num2str(time(4)),'：',num2str(time(5)));
+disp(['已经完成重命名，结束时间：',num2str(t2)]);
+diary off
